fda238bd969bd98ad59f0420783d22853f58eb3b
rizzsv <rizq.syafriano@gmail.com>
2025-01-21 06:12:21 +0700

    Resolve merge conflict

--- src/app/dashboard/(home)/flights/page.tsx
+++ src/app/dashboard/(home)/flights/page.tsx
@@ -5,6 +5,7 @@ import React, { type FC } from "react";
 import { DataTable } from "@/components/ui/data-table";
 import { Plus } from "lucide-react";
 import { columns } from "./components/columns-flight";
+import { getFlights } from "./lib/data";
 
 // interface FlightPageProps {
 
@@ -14,19 +15,22 @@ export const metada: Metadata = {
   title: "Dashboard | Flights",
 };
 
-const FlightPage: FC = () => {
+const FlightPage: FC = async () => {
+
+  const data = await getFlights()
+
   return (
     <>
       <div className="flex flex-row items-center justify-between">
         <div className="my-5 text-2xl font-bold">Flights</div>
         <Button asChild>
-          <Link href={"/dashboard/airplanes/create"}>
+          <Link href={"/dashboard/flights/create"}>
             <Plus className="mr-2 h-4 w-4" />
             Tambah Data
           </Link>
         </Button>
       </div>
-      <DataTable columns={ columns } data={[]} />
+      <DataTable columns={ columns } data={data} />
     </>
   );
 };
