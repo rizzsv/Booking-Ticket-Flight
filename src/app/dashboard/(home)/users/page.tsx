ae336c4b85f9daf7955c15406a4ed20fda9add76
rizzsv <rizq.syafriano@gmail.com>
2025-01-23 07:20:30 +0700

    slicing ui datable and integrate data for dashboard users

--- src/app/dashboard/(home)/users/page.tsx
+++ src/app/dashboard/(home)/users/page.tsx
@@ -1,24 +1,24 @@
 import { DataTable } from '@/components/ui/data-table'
 import React from 'react'
 import { Metadata } from 'next'
-import { columns } from './components/columns-ticket'
-import { getTickets } from './lib/data'
+import { columns } from './components/columns-users'
+import { getCustomers } from './lib/data'
 
 export const metada: Metadata = {
-    title: 'Dashboard | Tickets'
+    title: 'Dashboard | Users'
 }
 
 export default async function TicketPage() {
-    const tickets = await getTickets()
+    const users = await getCustomers()
 
 
     return (
         <>
 
         <div className='flex flex-row items-center justify-between'>
-            <div className='my-5 text-2xl font-bold'>Ticket</div>
+            <div className='my-5 text-2xl font-bold'>Users</div>
         </div>
-        <DataTable columns={columns} data={tickets}/>
+        <DataTable columns={columns} data={users}/>
         </>
     )
 }
